c63761a5a6361d3d889f4ce367e1fe61fa44afe7
HancockJ <hancockjack99@gmail.com>
2024-11-30 23:31:40 -0600

    day1

---
+++ day1.rb
@@ -0,0 +1,33 @@
+def day1_1()
+  a,b = get_lists()
+  total_diff = 0
+  for i in 0..a.length
+    total_diff += (a[i].to_i - b[i].to_i).abs
+  end
+  total_diff
+end
+
+def day1_2()
+  a,b = get_lists()
+  total_similarity = 0
+  for x in a
+    multiplier = 0
+    for y in b
+      if x == y
+        multiplier += 1
+      end
+    end
+    total_similarity += x * multiplier
+  end
+  total_similarity
+end
+
+def get_lists()
+  a, b = [], []
+  File.foreach('day1_input') do |line|
+    tmp = line.chomp.split("   ")
+    a << tmp[0].to_i
+    b << tmp[1].to_i
+  end
+  a,b = a.sort, b.sort
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative "day1.rb"
+print "Day 1 part 1", "\n"
+puts day1_1()
+print "Day 1 part 2", "\n"
+puts day1_2()
